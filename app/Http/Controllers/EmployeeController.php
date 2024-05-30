8f2ccf3ecc09b81d73b88da4e5e9b6b64fdab146
Tobiasz Tonn <toneq@tlen.pl>
2024-05-30 21:25:42 +0200

    Dodanie requesta w EmployeeController oraz walidacja wraz z sprawdzaniem

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EmployeeController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+    }
 }
