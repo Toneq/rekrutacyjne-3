848ad3c326c9be111343f9c035dbfa5f8ac476c7
Tobiasz Tonn <toneq@tlen.pl>
2024-05-30 21:32:09 +0200

    implementacja WorktimeService w WorktimeController. Zapisanie parametrów podanych w zadaniu

--- app/Http/Controllers/WorktimeController.php
+++ app/Http/Controllers/WorktimeController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\WorktimeService;
 
 class WorktimeController extends Controller
 {
-    //
+    protected $worktimeService;
+
+    public function __construct(WorktimeService $worktimeService){
+        $this->worktimeService = $worktimeService;
+        $this->overtimeRatePercent = 200;
+        $this->normMonthlyHours = 40;
+        $this->hourlyRate = 20;
+    }
 }
