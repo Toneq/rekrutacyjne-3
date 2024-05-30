d07aaacf222b569283e0b0a91b7961abef250964
Tobiasz Tonn <toneq@tlen.pl>
2024-05-30 21:36:31 +0200

    Dodanie register do tworzenia przedziałów pracy w danym dniu. Na ten moment odbieranie danych wraz z ich walidacja (sprawdzenie czy pracownik istnieje) i pobieranie jego danych

--- app/Http/Controllers/WorktimeController.php
+++ app/Http/Controllers/WorktimeController.php
@@ -15,4 +15,19 @@ class WorktimeController extends Controller
         $this->normMonthlyHours = 40;
         $this->hourlyRate = 20;
     }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'uuid' => 'required|string|exists:employees,uuid',
+            'data_rozpoczecia' => 'required|date_format:Y-m-d H:i',
+            'data_zakonczenia' => 'required|date_format:Y-m-d H:i|after:data_rozpoczecia',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $employee = Employee::where('uuid', $request->uuid)->first();
+    }
 }
