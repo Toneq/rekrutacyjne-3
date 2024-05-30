aab711cffa1b123e36aa362687cfbb3430d42f94
Tobiasz Tonn <toneq@tlen.pl>
2024-05-30 21:28:44 +0200

    Dodanie tworzenie pracownika i przetestowanie

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Employee;
+use Ramsey\Uuid\Uuid;
 
 class EmployeeController extends Controller
 {
@@ -16,6 +18,16 @@ class EmployeeController extends Controller
 
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $employee = new Employee();
+        $employee->uuid = Uuid::uuid4();
+        $employee->firstname = $request->firstname;
+        $employee->lastname = $request->lastname;
+        if ($employee->save()) {
+            return response()->json(['message' => 'Pracownik został dodany!', 'uuid' => $employee->uuid], 201);
+        } else {
+            return response()->json(['error' => 'Wystąpił błąd podczas dodawania czasu pracy'], 500);
         }
     }
 }
